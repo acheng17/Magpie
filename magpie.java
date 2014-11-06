f31a0e02ad1f0da8f8f2cf75c6cafdf2b7c61d16
acheng17 <acheng17@14ac02mxrumg083.punahou.edu>
2014-11-06 12:58:01 -1000

    activity three commit
    ~Better keyword detection~ Edited according to activity 3 in the magpie
    student lab.

--- magpie.java
+++ magpie.java
@@ -35,19 +35,19 @@ public class Magpie
   {
     response = "Are you still there?";
  }
-  else if (statement.indexOf("no") >= 0)
+  else if (findKeyword(statement, "no", 0) >= 0)
   {
    response = "Why so negative?";
   }
-  else if (statement.indexOf("mother") >= 0
-    || statement.indexOf("father") >= 0
-    || statement.indexOf("sister") >= 0
-    || statement.indexOf("brother") >= 0)
+  else if ((findKeyword(statement, "mother", 0) >= 0)
+    || findKeyword(statement, "father", 0) >= 0
+    || findKeyword(statement, "sister", 0) >= 0
+    || findKeyword(statement, "brother", 0) >= 0)
   {
    response = "Tell me more about your family.";
   }
-  else if (statement.indexOf("dog") >= 0 
-            || statement.indexOf("cat") >= 0 )
+  else if ((findKeyword(statement, "dog", 0) >= 0)
+            || findKeyword(statement, "cat", 0) >= 0)
              {
     response = "Tell me more about your pets.";
   }
@@ -56,18 +56,18 @@ public class Magpie
   //I have to code something so that it catches and react to the first order keyword, 
   //in this case, dog and reply using "Tell me about your pets."
   
-  else if (statement.indexOf("Mr. Kiang") >= 0
-             || statement.indexOf("Mr. Landgraf") >= 0)
+  else if ((findKeyword(statement, "Mr. Kiang", 0) >= 0)
+             || findKeyword(statement, "Mr. Landgraf", 0) >= 0)
   {
     response = "He sounds like a nice teacher.";
   }
-  else if (statement.indexOf("hungry") >= 0
-             || statement.indexOf("food") >= 0)
+  else if ((findKeyword(statement, "hungry", 0) >= 0)
+             || findKeyword(statement, "food", 0) >= 0)
   {
     response = "What kind of food do you like to eat?";
   }
-  else if (statement.indexOf("computer") >= 0
-             || statement.indexOf("phone") >= 0)
+  else if ((findKeyword(statement, "computer", 0) >= 0
+             || findKeyword(statement, "phone", 0) >= 0))
   {
     response = "What do you usually do on that? Do you play any games?";
   }
@@ -115,5 +115,54 @@ public class Magpie
   }
 
   return response;
+ }
+private int findKeyword(String statement, String goal,
+   int startPos)
+ {
+  String phrase = statement.trim();
+  // The only change to incorporate the startPos is in
+  // the line below
+  int psn = phrase.toLowerCase().indexOf(
+    goal.toLowerCase(), startPos);
+
+  // Refinement--make sure the goal isn't part of a
+  // word
+  while (psn >= 0)
+  {
+   // Find the string of length 1 before and after
+   // the word
+   String before = " ", after = " ";
+   if (psn > 0)
+   {
+    before = phrase.substring(psn - 1, psn)
+      .toLowerCase();
+   }
+   if (psn + goal.length() < phrase.length())
+   {
+    after = phrase.substring(
+      psn + goal.length(),
+      psn + goal.length() + 1)
+      .toLowerCase();
+   }
+
+   // If before and after aren't letters, we've
+   // found the word
+   if (((before.compareTo("a") < 0) || (before
+     .compareTo("z") > 0)) // before is not a
+           // letter
+     && ((after.compareTo("a") < 0) || (after
+       .compareTo("z") > 0)))
+   {
+    return psn;
+   }
+
+   // The last position didn't work, so let's find
+   // the next, if there is one.
+   psn = phrase.indexOf(goal.toLowerCase(),
+     psn + 1);
+
+  }
+
+  return -1;
  }
 }
